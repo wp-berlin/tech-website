03d9cb91b87621dd4bb2f7f4f5cccb2a14a5d19c
alpipego <alpipego@gmail.com>
2018-04-23 11:06:29 +0200

    add local debugging function

--- web/mu-plugins/debug.php
+++ web/mu-plugins/debug.php
@@ -5,3 +5,19 @@
  * Date: 23.04.18
  * Time: 10:55
  */
+
+add_action('wp_footer', function () {
+    if (WP_ENV === 'local' && isset($_GET['debug'])) {
+        $bugs  = explode(',', $_GET['debug']);
+        $bugs  = array_map('trim', $bugs);
+        $debug = [];
+        if (in_array('template', $bugs)) {
+            global $template;
+            $debug['template'] = str_replace(BASE_PATH, '', $template);
+        }
+
+        echo '<code><pre>';
+        var_dump($debug);
+        echo '</pre></code>';
+    }
+});
