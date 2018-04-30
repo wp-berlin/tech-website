d3c5e604a02ee38c22ed41941f1741eb2eea70f6
alpipego <alpipego@gmail.com>
2018-04-30 13:51:01 +0200

    initial meetup plugin version

--- web/plugins/meetup-api/meetup-api.php
+++ web/plugins/meetup-api/meetup-api.php
@@ -51,34 +51,39 @@ foreach ($eventIds as $eventId) {
         continue;
     }
     $body = json_decode($req->body);
-    if ($body->status !== 'upcoming') {
-        //        continue;
+    if ($body->status !== 'upcoming' || $body->visibility !== 'public') {
+        continue;
     }
-
 
     add_action('wpberlin/website/front_page', function () use ($body) {
         $dateObj = DateTime::createFromFormat('U', substr($body->time, 0, -3));
-//        $dateObj->setTimeZone(new DateTimeZone('Europe/Berlin'));
-        $int = new DateInterval(sprintf('PT%dS', $body->duration));
+        $dateObj->setTimeZone(new DateTimeZone('Europe/Berlin'));
+        $int = new DateInterval(sprintf('PT%dS', substr($body->duration, 0, -3)));
         ?>
         <div class="single-event">
-            <div class="single-event-meta">
-                <div class="single-event-meta-date">
+            <h2 class="single-event-title"><?= $body->name; ?></h2>
+            <div class="single-event-content single-main">
+                <?= $body->description; ?>
+            </div>
+            <div class="single-event-sidebar single-sidebar">
+                <div class="single-event-date">
                     <?= $dateObj->format('l, M d, Y'); ?>
-                    <div class="single-event-meta-date-time">
+                    <div class="single-event-time">
                         <?= sprintf('%s to %s', $dateObj->format('h:i A'), $dateObj->add($int)->format('h:i A')); ?>
                     </div>
                 </div>
-            </div>
-            <h2 class="single-event-title single-title"><?= $body->name; ?></h2>
-            <div class="single-event-content single-content">
-                <?= $body->description; ?>
+                <div class="single-event-location">
+                    <h3><?= $body->venue->name; ?></h3>
+                    <div class="single-event-location-address">
+                        <?= $body->venue->address_1; ?> &middot; <?= $body->venue->city; ?>
+                        <?php if (property_exists($body, 'how_to_find_us')) : ?>
+                            <p><?= $body->how_to_find_us; ?></p>
+                        <?php endif; ?>
+                    </div>
+                </div>
+                <a href="<?= $body->link; ?>" class="single-event-link">Join on Meetup.com</a>
             </div>
         </div>
         <?php
-        echo '<code><pre>';
-        var_dump($body);
-        echo '</pre></code>';
-
     });
 }
