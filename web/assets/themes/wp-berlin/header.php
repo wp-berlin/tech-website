bf538a6915478ef17f6ea96592e9b05d9f934621
alpipego <alpipego@gmail.com>
2018-05-31 10:41:06 +0200

    add favicon and touch icons, fixes #13

--- web/assets/themes/wp-berlin/header.php
+++ web/assets/themes/wp-berlin/header.php
@@ -13,6 +13,14 @@
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
+    <link rel="apple-touch-icon" sizes="180x180" href="/assets/img/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/assets/img/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/assets/img/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+    <link rel="mask-icon" href="/assets/img/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#202435">
+    <meta name="theme-color" content="#ffffff">
+
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
