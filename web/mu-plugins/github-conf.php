a5245255a3aa367244aedd6f8c7e4047c60cb858
alpipego <alpipego@gmail.com>
2018-06-14 16:02:14 +0200

    add metadata config, to github conf

--- web/mu-plugins/github-conf.php
+++ web/mu-plugins/github-conf.php
@@ -29,12 +29,30 @@ add_filter('ghcp/private_key', function () use ($conf) {
     return trim(file_get_contents(BASE_PATH . '/' . $conf->get('ghcp.private_key')));
 });
 
-add_filter('ghcp/rewrite/slug', function() {
+add_filter('ghcp/rewrite/slug', function () {
     return 'meeting-minutes';
 });
 
-add_filter('ghcp/post_type_object', function(PostType $post_type) {
-     $post_type->has_archive(true);
+add_filter('ghcp/post_type_object', function (PostType $post_type) {
+    $post_type->has_archive(true);
 
-     return $post_type;
+    return $post_type;
+});
+
+add_filter('ghcp/meta/data', function (array $meta) : array {
+    $addMeta = [
+        [
+            'key'   => 'date',
+            'regex' => '/^\d{4}(?:-\d{2}){2}T\d{2}:\d{2}$/',
+        ],
+        [
+            'key' => 'location',
+        ],
+        [
+            'key'   => 'location-search',
+            'regex' => '/^https?:\/\/.+$/',
+        ],
+    ];
+
+    return array_merge($meta, $addMeta);
 });
